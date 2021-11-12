8c82f88090208d5e7f777f7d79960769c3082f64
easbar <easbar.mail@posteo.net>
2021-11-12 22:59:06 +0100

    Revert "try left click"
    This reverts commit b34e57239c17c2d813c3f951f884ac5a24017093.

--- src/layers/ContextMenu.tsx
+++ src/layers/ContextMenu.tsx
@@ -23,11 +23,17 @@ export default function ({ map, queryPoints }: ContextMenuProps) {
         })
         setOverlay(overlay)
         map.addOverlay(overlay)
-        map.on('singleclick', e => {
-            const coordinate = e.coordinate
-            const lonLat = toLonLat(coordinate)
-            setMenuCoordinate({ lng: lonLat[0], lat: lonLat[1] })
-            overlay.setPosition(coordinate)
+        map.once('change:target', () => {
+            // we cannot listen to right-click simply using map.on('contextmenu') and need to add the listener to
+            // the map container instead
+            // https://github.com/openlayers/openlayers/issues/12512#issuecomment-879403189
+            map.getTargetElement().addEventListener('contextmenu', e => {
+                e.preventDefault()
+                const coordinate = map.getEventCoordinate(e)
+                const lonLat = toLonLat(coordinate)
+                setMenuCoordinate({ lng: lonLat[0], lat: lonLat[1] })
+                overlay.setPosition(coordinate)
+            })
         })
     }, [map])
     return (
