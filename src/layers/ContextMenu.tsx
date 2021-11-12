b34e57239c17c2d813c3f951f884ac5a24017093
easbar <easbar.mail@posteo.net>
2021-11-12 22:47:45 +0100

    try left click

--- src/layers/ContextMenu.tsx
+++ src/layers/ContextMenu.tsx
@@ -23,17 +23,11 @@ export default function ({ map, queryPoints }: ContextMenuProps) {
         })
         setOverlay(overlay)
         map.addOverlay(overlay)
-        map.once('change:target', () => {
-            // we cannot listen to right-click simply using map.on('contextmenu') and need to add the listener to
-            // the map container instead
-            // https://github.com/openlayers/openlayers/issues/12512#issuecomment-879403189
-            map.getTargetElement().addEventListener('contextmenu', e => {
-                e.preventDefault()
-                const coordinate = map.getEventCoordinate(e)
-                const lonLat = toLonLat(coordinate)
-                setMenuCoordinate({ lng: lonLat[0], lat: lonLat[1] })
-                overlay.setPosition(coordinate)
-            })
+        map.on('singleclick', e => {
+            const coordinate = e.coordinate
+            const lonLat = toLonLat(coordinate)
+            setMenuCoordinate({ lng: lonLat[0], lat: lonLat[1] })
+            overlay.setPosition(coordinate)
         })
     }, [map])
     return (
